c309210ef1f5bcf1979a92c1d28a4b90aec327d6
LeraRad <exlab7@gmail.com>
2023-01-12 20:30:43 +0100

    New, Create and Index actions was added to the Rules controller

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,2 +1,23 @@
 class RulesController < ApplicationController
+  def index
+    @rule = Rule.all
+  end
+
+  def new
+    @rule = Rule.new
+  end
+
+  def create
+    @rule = Rule.new(params.require(:rule).permit(:title,:body))
+
+    respond_to do |format|
+      if @rule.save
+        format.html { redirect_to rules_path, notice: "New Rule was successfully added." }
+        format.json { render :show, status: :created, location: @rule }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @rule.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end

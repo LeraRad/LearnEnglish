04478045a3827252e5dcb2865b358c0268c7b677
LeraRad <exlab7@gmail.com>
2023-01-19 08:39:53 +0100

    Controller for rules was created. Still it need some changes

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -20,4 +20,38 @@ class RulesController < ApplicationController
       end
     end
   end
+
+  def edit
+    @rule = Rule.find(params[:id])
+  end 
+
+def update
+  @rule = Rule.find(params[:id])
+  respond_to do |format|
+    if @rule.update(params.require(:rule).permit(:title,:body))
+      format.html { redirect_to rules_path, notice: "The rule was successfully updated." }
+      format.json { render :show, status: :ok, location: @rule }
+    else
+      format.html { render :edit, status: :unprocessable_entity }
+      format.json { render json: @rule.errors, status: :unprocessable_entity }
+    end
+  end
+end
+
+def show
+  @rule = Rule.find(params[:id])
+end
+
+def destroy
+  # Perform the lookup
+  @rule = Rule.find(params[:id])
+
+  #Destroy/delete the record
+  @rule.destroy
+
+  #Redirect
+  respond_to do |format|
+    format.html { redirect_to words_url, notice: "The rule #{@rule.title} was removed." }
+  end
+end
 end

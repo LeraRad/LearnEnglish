b8851acc544b9b3ed8b9e8478671f4d8879b3b11
LeraRad <exlab7@gmail.com>
2023-02-09 23:50:42 +0100

    Have created the Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  root to: 'words#index'
+  root to: 'homepage#index'
   get 'translation', to: 'translations#index'
   post 'translations/translate', as: :translate
   resources :texts
